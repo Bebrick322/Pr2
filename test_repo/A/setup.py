f08c48b533473abc9aa810aab8deb266b12bf591
Bebrick322 <resulov_eldar06@mail.ru>
2025-11-20 19:54:36 +0300

    feat: Реализованы этапы 3, 4 и 5 (построение графа, топологическая сортировка и визуализация).

---
+++ test_repo/A/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='test-package-A',
+    version='1.0.0',
+    description='Корневой пакет для тестирования',
+
+    install_requires=[
+        "B",
+
+
+        
+    ]
+)
---
+++ test_repo/D/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="D",
+    version="1.0.0",
+    install_requires=[
+        "B",
+        "E"
+        ]
+)
